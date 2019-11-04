181bd6a81a5f0da9045c43f764ee2ab40b6fb7fc
iyb-guomaosen <1506299948@qq.com>
2019-11-04 10:47:38 +0800

    添加@babel/plugin-proposal-class-properties插件

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -44,6 +44,7 @@ module.exports = {
                             '@babel/preset-react',
                         ],
                         plugins: [
+                            '@babel/plugin-proposal-class-properties',
                             '@babel/plugin-syntax-dynamic-import',
                             '@loadable/babel-plugin',
                             'lodash'
